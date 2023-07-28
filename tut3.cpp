b243f844b304b6160d43dd079cc6a92f0cc58b2c
Harshaerror <140278226+Harshaerror@users.noreply.github.com>
2023-07-28 23:12:47 +0530

    Add files via upload

---
+++ tut3.cpp
@@ -0,0 +1,18 @@
+#include<iostream>//Input output stream
+using namespace std;
+int glo = 9;
+void sum(){
+    int a;
+    cout<<glo;
+}
+int main(){
+    int glo = 88;
+    int a = 5,b=6;
+
+    cout<<a<<"\n";
+    cout<<b<<"\n";
+    sum();
+    cout<<"\n"<<glo;
+
+    return 0;
+} 
