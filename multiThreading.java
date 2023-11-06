90421ebcf09ab941374c4bba03fc9fc8e81f3fb5
Harshaerror <140278226+Harshaerror@users.noreply.github.com>
2023-11-06 22:57:38 +0530

    To check threads priority getPriority() To set threads priority setPriority() Main() i.e-> mainthread always have priority of 5 Threads priority are range in 1 to 10 where 1 is lowest and 10 is highest

--- multiThreading.java
+++ multiThreading.java
@@ -1,11 +1,53 @@
-class mythreads extends Thread 
-{}
-class multiThreading{
-    public static void main(String[] args){
-        System.out.println(Thread.currentThread().getPriority());
-        Thread.currentThread().setPriority(6);
-        mythreads m = new mythreads();
-        System.out.println(Thread.currentThread().getPriority());
+// class mythreads extends Thread 
+// {}
+// class multiThreading{
+//     public static void main(String[] args){
+//         System.out.println(Thread.currentThread().getPriority());
+//         Thread.currentThread().setPriority(6);
+//         mythreads m = new mythreads();
+//         System.out.println(Thread.currentThread().getPriority());
 
+//     }
+// } 
+
+
+
+// class multiThreading{
+//     public static void main(String[] args){
+//         System.out.println(Thread.currentThread().getPriority());
+//       Thread.currentThread().setPriority(6);
+
+//     10 is high priority and 1 is low priority
+
+//        try{ Thread.currentThread().setPriority(15);
+//         }
+//         catch(IllegalArgumentException e){
+//     System.out.println("running thread always in between 1 to 10");
+//         }
+//        mythreads m = new mythreads();
+//         System.out.println(m.getPriority());
+
+//     }
+// } 
+
+
+
+class mythreads extends Thread{
+    public void run() {
+        for(int i=0;i<=10;i++){
+            System.out.println("child thread");
+        }
     }
-} 
+}
+
+
+class multiThreading{
+public static void main(String[] args)
+{
+    mythreads m =new mythreads();
+    m.setPriority(10);
+    m.start();
+    for(int i=0;i<=10;i++){
+    System.out.println("main thread");
+    }
+}}
