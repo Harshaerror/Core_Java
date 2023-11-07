fc7255b07e2f99334f684e8979d86f6f397c7c38
Harshaerror <140278226+Harshaerror@users.noreply.github.com>
2023-11-07 16:09:37 +0000

    mythread.mt = Thread.currentThread(); using instance of mythread() class Static Variable invoke in a current main() thread

--- multiThreading.java
+++ multiThreading.java
@@ -52,22 +52,80 @@
 
 
 
-class mythreads extends Thread{
-    public void run(){
-        for(int i=0;i<10;i++){
-            Thread.yield();
-            System.out.println("child Thread");
+// class mythreads extends Thread{
+//     public void run(){
+//         for(int i=0;i<10;i++){
+//             Thread.yield();
+//             System.out.println("child Thread");
             
+//         }
+//     }
+// }
+// class multiThreading{
+//     public static void main(String [] args){
+//         mythreads m = new mythreads();
+//         m.start();
+//         for(int i=0;i<10;i++){
+//             System.out.println("Main Thread");
+//         }
+
+//     }
+// }
+
+
+// class mythread extends Thread {
+   
+// public void run(){
+//         for(int i=0;i<10;i++){
+//         System.out.println("Ramam");
+        
+//         try{
+//             Thread.sleep(300);
+//         }
+//         catch(InterruptedException e){
+//                                }
+//         }
+//     }
+// }
+// class multiThreading {
+//     public static void main(String [] args) throws InterruptedException{
+//     mythread s = new mythread();     
+//      s.start();
+//      s.join(1000);
+     
+//      for(int i=0;i<10;i++){
+//         System.out.println("Raghvamm");
+//      }
+//     }
+// }
+
+//this TWO codes are works same but differently upper one is using time and
+// lowesr's using static variable and currentThread() call
+
+
+class mythread extends Thread {
+   static Thread mt;
+public void run(){
+        for(int i=0;i<10;i++){
+        System.out.println("Ramam");
+        
+        try{
+            mt.join(2000);
+        }
+        catch(InterruptedException e){
+                               }
         }
     }
 }
-class multiThreading{
-    public static void main(String [] args){
-        mythreads m = new mythreads();
-        m.start();
-        for(int i=0;i<10;i++){
-            System.out.println("Main Thread");
-        }
+class multiThreading {
+    public static void main(String [] args) throws InterruptedException{
+    mythread.mt = Thread.currentThread();
 
+    mythread s = new mythread();     
+     s.start();
+    for(int i=0;i<10;i++){
+        System.out.println("Raghvamm");
+        Thread.sleep(200);
+     }
     }
 }
